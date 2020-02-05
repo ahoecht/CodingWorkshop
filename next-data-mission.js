d52d4d641c8d98612aaf8fec607a48d57169ba98
Alex Hoecht <ahoecht@cisco.com>
2020-02-05 08:56:30 -0500

    Fixing inital commit of dnac code, and completeing mission

---
+++ next-data-mission.js
@@ -0,0 +1,226 @@
+/*DO NOT EDIT - NeXt Topology file generated from DNA Center Device and Module Inventory*/
+
+var topologyData = 
+{
+    "links": [
+        {
+            "source": 0,
+            "target": 1
+        },
+        {
+            "source": 0,
+            "target": 2
+        },
+        {
+            "source": 0,
+            "target": 3
+        },
+        {
+            "source": 0,
+            "target": 4
+        },
+        {
+            "source": 0,
+            "target": 5
+        },
+        {
+            "source": 0,
+            "target": 6
+        },
+        {
+            "source": 0,
+            "target": 7
+        },
+        {
+            "source": 0,
+            "target": 8
+        },
+        {
+            "source": 0,
+            "target": 9
+        },
+        {
+            "source": 10,
+            "target": 11
+        },
+        {
+            "source": 10,
+            "target": 12
+        },
+        {
+            "source": 13,
+            "target": 14
+        },
+        {
+            "source": 13,
+            "target": 15
+        },
+        {
+            "source": 16,
+            "target": 17
+        },
+        {
+            "source": 16,
+            "target": 18
+        }
+    ],
+    "nodes": [
+        {
+            "icon": "router",
+            "id ": 0,
+            "name": "asr1001-x.abc.inc",
+            "platform": "ASR1001-X",
+            "serial": "FXS1932Q1SE",
+            "x": 0,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 1,
+            "name": "Cisco ASR1001-X Embedded Services Processor",
+            "serial": "FXS1932Q1SE",
+            "x": 20,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 2,
+            "name": "Cisco ASR1001-X Route Processor",
+            "serial": "FXS1932Q1SE",
+            "x": 40,
+            "y": 60
+        },
+        {
+            "icon": "server",
+            "id ": 3,
+            "name": "Unknown pluggable optics",
+            "serial": "FXS1932Q1SE",
+            "x": 60,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 4,
+            "name": "GE T",
+            "serial": "FXS1932Q1SE",
+            "x": 80,
+            "y": 100
+        },
+        {
+            "icon": "server",
+            "id ": 5,
+            "name": "Built-in GE SPA with 2 port 10G and 6 port 1G Interfaces",
+            "serial": "FXS1932Q1SE",
+            "x": 100,
+            "y": 120
+        },
+        {
+            "icon": "server",
+            "id ": 6,
+            "name": "GE T",
+            "serial": "FXS1932Q1SE",
+            "x": 120,
+            "y": 140
+        },
+        {
+            "icon": "server",
+            "id ": 7,
+            "name": "GE T",
+            "serial": "FXS1932Q1SE",
+            "x": 140,
+            "y": 160
+        },
+        {
+            "icon": "server",
+            "id ": 8,
+            "name": "Unknown pluggable optics",
+            "serial": "FXS1932Q1SE",
+            "x": 160,
+            "y": 180
+        },
+        {
+            "icon": "server",
+            "id ": 9,
+            "name": "Cisco ASR1001-X SPA Interface Processor",
+            "serial": "FXS1932Q1SE",
+            "x": 180,
+            "y": 200
+        },
+        {
+            "icon": "switch",
+            "id ": 10,
+            "name": "cat_9k_1.abc.inc",
+            "platform": "C9300-24UX",
+            "serial": "FCW2136L0AK",
+            "x": 200,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 11,
+            "name": "Switch 1 - C9300-24UX - Fixed Module 0",
+            "serial": "FCW2136L0AK",
+            "x": 220,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 12,
+            "name": "8x10G Uplink Module",
+            "serial": "FCW2136L0AK",
+            "x": 240,
+            "y": 60
+        },
+        {
+            "icon": "switch",
+            "id ": 13,
+            "name": "cat_9k_2.abc.inc",
+            "platform": "C9300-24UX",
+            "serial": "FCW2140L039",
+            "x": 260,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 14,
+            "name": "Switch 1 - C9300-24UX - Fixed Module 0",
+            "serial": "FCW2140L039",
+            "x": 280,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 15,
+            "name": "8x10G Uplink Module",
+            "serial": "FCW2140L039",
+            "x": 300,
+            "y": 60
+        },
+        {
+            "icon": "switch",
+            "id ": 16,
+            "name": "cs3850.abc.inc",
+            "platform": "WS-C3850-48U-E",
+            "serial": "FOC1833X0AR",
+            "x": 320,
+            "y": 80
+        },
+        {
+            "icon": "server",
+            "id ": 17,
+            "name": "Switch 1 - WS-C3850-48U - Fixed Module 0",
+            "serial": "FOC1833X0AR",
+            "x": 340,
+            "y": 40
+        },
+        {
+            "icon": "server",
+            "id ": 18,
+            "name": "4x10G Uplink Module",
+            "serial": "FOC1833X0AR",
+            "x": 360,
+            "y": 60
+        }
+    ]
+}
+/*DO NOT EDIT - EOF*/
